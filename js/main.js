866210de4efa114f8b5cc34c45f4f4a0cf900ae5
ethan4496 <ts2a1996@gmail.com>
2025-09-12 18:50:37 +0700

    update animation

--- js/main.js
+++ js/main.js
@@ -22,32 +22,29 @@ jQuery(document).ready(function ($) {
     // topo
 
     let $wrapper = $(".scrollLoop");
-    // Nhân đôi nội dung để tạo hiệu ứng chạy liên tục
     $wrapper.each(function () {
         let $list = $(this).find('.loop-item');
         $list.append($list.html());
 
-        let speed = 50; // px/giây
+        let speed = 5000;
         let left = 0;
-        let listWidth = $list.width() / 2; // chỉ 1 nửa vì đã nhân đôi
+        let listWidth = $list.width() / 2;
     
         function loop() {
-            left -= 1; // mỗi bước dịch 1px
+            left -= 1;
             if (Math.abs(left) >= listWidth) {
-                left = 0; // reset lại khi hết 1 vòng
+                left = 0;
             }
             $list.css("transform", `translateX(${left}px)`);
         }
     
         let timer = setInterval(loop, 1000 / speed);
     
-        // Dừng khi hover
-        $wrapper.on("mouseenter", function () {
+        $list.on("mouseenter", function () {
             clearInterval(timer);
         });
     
-        // Chạy lại khi rời chuột
-        $wrapper.on("mouseleave", function () {
+        $list.on("mouseleave", function () {
             timer = setInterval(loop, 1000 / speed);
         });
     });
@@ -78,4 +75,95 @@ jQuery(document).ready(function ($) {
             ],
         });
     }
+    function animateCounter(element) {
+        const from = parseInt(element.getAttribute("data-from-value")) || 0;
+        const to = parseInt(element.getAttribute("data-to-value")) || 0;
+        const duration = parseInt(element.getAttribute("data-duration")) || 2000;
+
+        let startTime = null;
+
+        function updateCounter(currentTime) {
+            if (!startTime) startTime = currentTime;
+            const progress = Math.min((currentTime - startTime) / duration, 1);
+            let value = Math.floor(progress * (to - from) + from);
+            value = value.toLocaleString('en-US')
+            element.textContent = value;
+
+            if (progress < 1) {
+            requestAnimationFrame(updateCounter);
+            }
+        }
+
+        requestAnimationFrame(updateCounter);
+    }
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+            animateCounter(entry.target);
+            observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 });
+    document.querySelectorAll(".counter-number").forEach(el => {
+        observer.observe(el);
+    });
+    const observerAnimation = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+            entry.target.classList.add("animate");
+            // Nếu chỉ muốn chạy 1 lần:
+            observer.unobserve(entry.target);
+            }
+        });
+        }, { threshold: 0.3 });
+
+    document.querySelectorAll('.togo-fade-in-left').forEach(el => {
+        observerAnimation.observe(el);
+    });
+    document.querySelectorAll('.togo-fade-in-right').forEach(el => {
+        observerAnimation.observe(el);
+    });
+    document.querySelectorAll('.togo-fade-in-up').forEach(el => {
+        observerAnimation.observe(el);
+    });
+    document.querySelectorAll('.togo-fade-in-down').forEach(el => {
+        observerAnimation.observe(el);
+    });
+    jQuery('.header .nav-item').on('show.bs.dropdown', function(){
+        if(jQuery(window).width() < 768){
+            jQuery('.header .navbar-nav').css({transform: 'translateX(-100%)'})
+        }
+    })
+    jQuery('.mobie-menu-back').on('click', function(){
+        jQuery('.header .navbar-nav').css({transform: 'translateX(0)'})
+    })
+    jQuery('.btn-mb-menu').on('click', function(){
+        jQuery('.header').addClass('is-active');
+    })
+    jQuery('.mobile-menu-close').on('click', function(){
+        jQuery('.header').removeClass('is-active');
+        jQuery('.header-menu').removeClass('show');
+        jQuery('.header .navbar-nav').css({transform: 'translateX(0)'});
+        jQuery('.btn-mb-menu i').addClass('fa-bars').removeClass('fa-times')
+    })
+    jQuery('.img-overlay').on('click', function(){
+        jQuery('.dialog-light-box').fadeIn("slow")
+        jQuery('#video-intro').html(`
+            <iframe
+        src="https://www.youtube.com/embed/XHOmBV4js_E?feature=oembed&start&end&wmode=opaque&loop=0&controls=0&mute=0&rel=0&cc_load_policy=0"
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+        </iframe>`)
+    });
+    jQuery('.dialog-close-button').on('click', function(e){
+        console.log('cccc');
+        e.preventDefault();
+        jQuery('.dialog-light-box').fadeOut("slow")
+    })
+    jQuery('.footer-title').on('click', function(){
+        jQuery(this).toggleClass('show');
+        var sub = jQuery(this).parent().find('.footer-menu').toggleClass('show')
+    })
 });
