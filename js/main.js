87ed281113e5f7fd3416e27cd93adbcc3c6df831
ethan4496 <ts2a1996@gmail.com>
2025-09-23 19:00:58 +0700

    new code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,172 @@
+var slide_function = {
+    init: function () {
+        slide_function.gallery_slide();
+        slide_function.list_box_service();
+        slide_function.tours_releated();
+    },
+    gallery_slide: function () {
+        var slide = jQuery('.list-gallery .gallery-gird-slide');
+        var item_slide = jQuery('.list-gallery .gallery-gird-slide .item');
+
+        var slide2 = jQuery('.list-gallery .gallery-gird-slide2');
+        var item_slide2 = jQuery('.list-gallery .gallery-gird-slide2 .item');
+
+        if (slide2.length > 0 && item_slide2.length > 1) {
+            slide2.slick({
+                dots: false,
+                arrow: true,
+                speed: 1000,
+                autoplay: false,
+                autoplaySpeed: 5000,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                responsive: [
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: false,
+                            centerMode: false,
+                            arrows: true,
+                        },
+                    },
+                ],
+            });
+        }
+        if (slide.length > 0 && item_slide.length > 3) {
+            function updateSecond() {
+                slide.find('.is-second').removeClass('is-second');
+                var w = jQuery(window).width();
+                if (w > 1024) {
+                    var actives = slide.find('.slick-active');
+                    if (actives.length > 1) {
+                        actives.eq(1).addClass('is-second');
+                    }
+                }
+            }
+            slide.on('init reInit afterChange', function (event, slick, currentSlide) {
+                updateSecond();
+            });
+            slide.slick({
+                dots: false,
+                arrow: true,
+                speed: 1000,
+                autoplay: false,
+                autoplaySpeed: 5000,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                responsive: [
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: false,
+                            centerMode: false,
+                            arrows: true,
+                        },
+                    },
+                ],
+            });
+        }
+    },
+    list_box_service: function () {
+        var slide = jQuery('.list-box-service .list-service');
+        var item_slide = jQuery('.list-box-service .list-service .item');
+        if (slide.length > 0 && item_slide.length > 4) {
+            slide.slick({
+                dots: false,
+                arrow: true,
+                speed: 1000,
+                autoplay: false,
+                autoplaySpeed: 5000,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                responsive: [
+                    {
+                        breakpoint: 960,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: false,
+                            centerMode: false,
+                            arrows: true,
+                        },
+                    },
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                            infinite: true,
+                            dots: false,
+                            centerMode: false,
+                            arrows: true,
+                        },
+                    },
+                ],
+            });
+        }
+    },
+    tours_releated: function () {
+        var slide = jQuery('.tours-releated .list-tours');
+        var item_slide = jQuery('.tours-releated .list-tours .post');
+        if (slide.length > 0 && item_slide.length > 4) {
+            slide.slick({
+                dots: false,
+                arrow: false,
+                speed: 1000,
+                autoplay: false,
+                autoplaySpeed: 5000,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: false,
+                            centerMode: false,
+                            arrows: false,
+                        },
+                    },
+                    {
+                        breakpoint: 967,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                            infinite: true,
+                            dots: false,
+                            centerMode: false,
+                            arrows: false,
+                        },
+                    },
+                    {
+                        breakpoint: 575,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: false,
+                            centerMode: false,
+                            arrows: true,
+                        },
+                    },
+                ],
+            });
+        }
+    }
+}
 jQuery(document).ready(function ($) {
     var btn_menu = jQuery('.btn-mb-menu');
     if (btn_menu.length > 0) {
@@ -165,5 +334,194 @@ jQuery(document).ready(function ($) {
     jQuery('.footer-title').on('click', function(){
         jQuery(this).toggleClass('show');
         var sub = jQuery(this).parent().find('.footer-menu').toggleClass('show')
-    })
+    });
+    jQuery('.filter-item__top').on('click', function(){
+        var parent = jQuery(this).parents('.filter-item');
+        parent.toggleClass('active');
+        var content = parent.find('.filter-item__content');
+        content.stop(true, true).slideToggle(800);
+    });
+    jQuery('.show-more').on('click', function(e){
+        e.preventDefault();
+        var parent = jQuery(this).parents('.filter-item');
+        console.log(parent)
+        console.log(parent.find('.filter-checkbox'));
+        parent.find('.filter-checkbox.hide').toggleClass('show')
+    });
+    let tours_content = jQuery('.tours-content').length;
+    if(tours_content > 0){
+        const rangeSlider = document.querySelector('.range-slider');
+        const minInput = rangeSlider.querySelector('input[name="min_price"]');
+        const maxInput = rangeSlider.querySelector('input[name="max_price"]');
+        const inclRange = rangeSlider.querySelector('.incl-range');
+
+        function updateRange() {
+            let min = parseInt(jQuery('input[name="min_price"]').val());
+            let max = parseInt(jQuery('input[name="max_price"]').val());
+            let rangeMin = parseInt(jQuery('input[name="min_price"]').attr("min"));
+            let rangeMax = parseInt(jQuery('input[name="min_price"]').attr("max"));
+            console.log("min", min);
+            console.log("max", max);
+            let left = ((min - rangeMin) / (rangeMax - rangeMin)) * 100;
+            let right = ((max - rangeMin) / (rangeMax - rangeMin)) * 100;
+
+            inclRange.style.left = left + "%";
+            inclRange.style.width = (right - left) + "%";
+            jQuery('.incl-range').css({
+                left: left + "%",
+                width: (right - left) + "%"
+            })
+        }
+        jQuery('input[name="min_price"]').on('change', function(){
+            var val = jQuery(this).val();
+            jQuery('input[name="min_price"]').val(val);
+            updateRange();
+        })
+        jQuery('input[name="max_price"]').on('change', function(){
+            var val = jQuery(this).val();
+            jQuery('input[name="max_price"]').val(val);
+            updateRange();
+        })
+        // [minInput, maxInput].forEach(input => {
+        //     input.addEventListener("input", updateRange);
+        // });
+
+        updateRange();
+    }
+    jQuery('.togo-select').on('click', function(){
+        jQuery('.togo-select__content').toggle();
+    });
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.togo-select').length) {
+            $('.togo-select__content').hide();
+        }
+    });
+    $('.trip-video').hover(
+        function() {
+            $(this).addClass('playing');
+            $(this).find('video').get(0).play();
+        },
+        function() {
+            $(this).removeClass('playing');
+            $(this).find('video').get(0).pause();
+        }
+    );
+    jQuery('.open-filter-canvas').on('click', function(){
+        jQuery('.filter-canvas-wrapper').addClass('open');
+        jQuery('.layout-canvas').addClass('open')
+    });
+    jQuery('.filter-canvas-overlay').on('click', function(){
+        jQuery('.filter-canvas-wrapper').removeClass('open');
+        jQuery('.layout-canvas').removeClass('open')
+    });
+    if(slide_function){
+        slide_function.init();
+    }
+    
+    const boxReadMore= document.querySelector('.box-readmore');
+    if(boxReadMore){
+        boxReadMore.addEventListener('click', function (e) {
+            e.preventDefault();
+            const box = document.querySelector('.box-content');
+            box.classList.toggle('expanded');
+            this.textContent = box.classList.contains('expanded') ? "Read less" : "Read more";
+        });
+    }
+    $('.openCalendar').each(function (index) {
+        var $input = $(this);
+        var $hiddenInput = $input.siblings('.hiddenDate');
+        var $displayText = $input.find('.selectedDateText');
+        var isMobile = $(window).width() < 768;
+        console.log('cccc')
+        new Litepicker({
+            element: this,
+            singleMode: true,
+            numberOfMonths: isMobile ? 1 : 2,
+            numberOfColumns: isMobile ? 1 : 2,
+            format: 'YYYY-MM-DD',
+            position: 'center',
+            minDate: new Date(),
+            setup: function (picker) {
+                picker.on('render', function () {
+                    $('.litepicker .day-item').each(function () {
+                        var $day = $(this);
+                        if (!$day.find('.price').length && !$day.hasClass('is-empty')) {
+                            $('<span class="price">$50</span>').appendTo($day);
+                        }
+                    });
+                });
+
+                picker.on('selected', function (date) {
+                    
+                    var value = date.format('MMM DD, YYYY');
+                    $hiddenInput.val(value);
+                    $displayText.text(value);
+                });
+            }
+        });
+    });
+    $('.form-input').each(function () {
+        var $form = $(this);
+        var $input = $form.find('input[type=number]');
+        var $plus = $form.find('.plus');
+        var $minus = $form.find('.minus');
+
+        $plus.on('click', function () {
+            var value = parseInt($input.val()) || 0;
+            value++;
+            $input.val(value);
+
+            if (value > 0) {
+                $minus.removeClass('disabled');
+            }
+        });
+
+        $minus.on('click', function () {
+            var value = parseInt($input.val()) || 0;
+            if (value > 0) {
+                value--;
+                $input.val(value);
+            }
+            if (value === 0) {
+                $minus.addClass('disabled');
+            }
+        });
+    });
+    const lightbox=GLightbox({
+        selector: '.glightbox',
+        touchNavigation: true,
+        autoplayVideos: true
+    });
+    $('.scroll-link').on('click',function(e) {
+        e.preventDefault();
+        const target=$(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - 150
+        },600);
+    });
+    flatpickr("#daterange", {
+        mode: "range",          // chọn khoảng ngày
+        dateFormat: "d-m-Y",    // format ngày
+        plugins: [ new confirmDatePlugin({ confirmText: "Apply" }) ],
+        showMonths: 2           // hiển thị 2 tháng cạnh nhau
+    });
+});
+
+
+document.querySelectorAll('.formguest').forEach(function(el, index) {
+    el.addEventListener('click', function(e) {
+        e.stopPropagation();
+        let box = el.parentElement.querySelector('.box-formguest');
+        document.querySelectorAll('.box-formguest').forEach(function(b) {
+            b.classList.remove('show-gues');
+        });
+        box.classList.toggle('show-gues');
+    });
+});
+
+document.addEventListener('click', function() {
+    document.querySelectorAll('.box-formguest').forEach(function(b) {
+        b.classList.remove('show-gues');
+    });
 });
